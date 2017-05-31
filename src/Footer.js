bf34a70e1dcd9e0950dca30f137b2a428d14a80b
Pablo Jurado <pjurado85@gmail.com>
2017-05-31 18:20:36 -0500

    create basic component for layout

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className='component'>this is the footer</footer>
+    );
+  }
+}
+
+export default Footer;
