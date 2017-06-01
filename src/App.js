80981b6914f74cc1493513a549cbe0f52809100b
Pablo Jurado <pjurado85@gmail.com>
2017-05-31 20:35:07 -0500

    add props to body component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,44 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Body from './Body';
 import Footer from './Footer';
+import JSON from './data'
 
-
-
-// import './app.css';
+// const api = 'https://api.github.com/users/'
+// const user = 'pablo-jurado'
+// const url = api + user
+//
+// var request = new XMLHttpRequest();
+// request.open('GET', url, true);
+//
+// request.onload = function() {
+//   if (request.status >= 200 && request.status < 400) {
+//     var data = JSON.parse(request.responseText);
+//     console.log(data)
+//   } else {
+//     console.log(request.responseText)
+//   }
+// }
+//
+// request.onerror = function(e) {
+//   console.log(e)
+// }
+//
+// request.send()
+//
+// const mainState = {
+//   isLoading: true,
+//   userName: user,
+//   userDate: null
+// }
+let gitObj = JSON
+console.log(gitObj)
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Header />
-        <Body />
+        <Body data={JSON.login}/>
         <Footer />
       </div>
     );
--- src/Body.js
+++ src/Body.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Body extends Component {
   render() {
     return (
-      <div className='component'>this is the body</div>
+      <div className='component'>this is the body {this.props.data}</div>
     );
   }
 }
